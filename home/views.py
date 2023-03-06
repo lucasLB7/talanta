1e6c53f2a7f454ca89ec2035330816b1cf4c8da4
LucasLB7 <plucaslambert@gmail.com>
2023-03-06 23:05:03 +0300

    Some changes made, templating infrastructure being done, still working on the implementation of the app

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home_page.html', {})
+    return render(request, 'main.html', {})
+    #return render(request, 'header_footer.html', {})
+    #return render(request, 'main.html', {})
 
 # Create your views here.
