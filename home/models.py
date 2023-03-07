b1f3f00d4b878f13c905d4c79b4296b0218286fe
LucasLB7 <plucaslambert@gmail.com>
2023-03-07 16:53:40 +0300

    Added image upload model to the admin section

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models import ImageField
 
 # Create your models here.
+
+# Create your models here.
+class Carousel_Item (models.Model):
+    caption = models.CharField(max_length=200,blank=False)
+    order = models.IntegerField()
+    carousel_image = models.ImageField(upload_to='media')
+    def __unicode__(self):
+        return self.caption
--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
-from home import views
+from django.urls import path, include
+from . import views
+
+app_name = "main"
 
 urlpatterns = [
     path('', views.home, name='home_page'),
