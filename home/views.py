f560d089f36cc5c2e74a1555ead385b9c36e2234
LucasLB7 <plucaslambert@gmail.com>
2023-03-07 14:40:45 +0300

    managed to link the blocks together and figured out something was not being registerd in my HTML code for a modal

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'main.html', {})
     #return render(request, 'header_footer.html', {})
-    #return render(request, 'main.html', {})
+    #return render(request, 'part.html', {})
 
 # Create your views here.
