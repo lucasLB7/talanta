07e9753c5224d050447e3145f10b5824bb573d7b
LucasLB7 <plucaslambert@gmail.com>
2023-03-08 13:31:11 +0300

    Added requirements.txt

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 def home(request):
     return render(request, 'main.html', {})
     #return render(request, 'header_footer.html', {})
